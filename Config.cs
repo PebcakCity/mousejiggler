389a96a769a0b3d5026effdefd3de62aea26fdc7
tannmatter <tannmatter@gmail.com>
2024-06-11 18:39:52 -0500

    Initial commit

---
+++ Config.cs
@@ -0,0 +1,55 @@
+﻿using System.Reflection;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+
+namespace PebcakCity.MouseJiggler
+{
+    public class Config
+    {
+        private string? Filename { get; set; }
+        private const string DefaultConfig = "MouseJiggler.json";
+
+        public bool JiggleOnStartup { get; set; } = false;
+        public bool MinimizeOnStartup { get; set; } = false;
+        public bool ZenJiggle { get; set; } = false;
+        public int JiggleInterval { get; set; } = 60;
+
+        public Config() { }
+        internal static Config Read(string filename = DefaultConfig)
+        {
+            try
+            {
+                using FileStream json = new (filename, FileMode.Open);
+                JsonNode? root = JsonNode.Parse(json);
+                Config config = JsonSerializer.Deserialize<Config>(root)!;
+                config.Filename = filename;
+                return config;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error reading config: {ex.Message}");
+                return new Config();
+            }
+        }
+
+        internal void Write()
+        {
+            string assemblyDir = System.AppContext.BaseDirectory;
+            this.Filename ??= Path.Combine(assemblyDir, DefaultConfig);
+            try
+            {
+                using FileStream json = new (this.Filename, FileMode.Create, FileAccess.Write);
+                var jsonOptions = new JsonSerializerOptions
+                {
+                    WriteIndented = true,
+                };
+                JsonSerializer.Serialize(json, this, jsonOptions);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error writing config: {ex}");
+            }
+        }
+    }
+}
---
+++ Helpers.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.InteropServices;
+using Windows.Win32.UI.Input.KeyboardAndMouse;
+using Windows.Win32;
+
+namespace PebcakCity.MouseJiggler
+{
+    internal static class Helpers
+    {
+        internal const uint AttachParentProcess = unchecked((uint)-1);
+        internal static void Jiggle( int delta )
+        {
+            var inp = new INPUT
+            {
+                type = INPUT_TYPE.INPUT_MOUSE,
+                Anonymous = new INPUT._Anonymous_e__Union
+                {
+                    mi = new MOUSEINPUT
+                    {
+                        dx = delta,
+                        dy = delta,
+                        mouseData = 0,
+                        dwFlags = MOUSE_EVENT_FLAGS.MOUSEEVENTF_MOVE,
+                        time = 0,
+                        dwExtraInfo = 0
+                    },
+                },
+            };
+            _ = PInvoke.SendInput([inp,], Marshal.SizeOf<INPUT>());
+        }
+    }
+}
